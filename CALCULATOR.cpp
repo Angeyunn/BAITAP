d490947c45d6ebb90103196720b6ef603c8ec70a
Nguyen Thai An <128191210+Angeyunn@users.noreply.github.com>
2025-03-03 11:38:11 +0700

    CALCULATOR.cpp

--- CALCULATOR.cpp
+++ CALCULATOR.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
 
-int main(){
-    int num1, num2; cin >> num1 >> num2;
-    char op; cin >> op;
-    switch (op)
-    {
-    case '+':
-        cout << num1 + num2;
-        break;
-    case '-':
-        cout << num1 - num2;
-        break;
-    case '*':
-        cout << num1 * num2;
-        break;
-    case '/':
-        cout << num1 / num2;
-        break;
-    case '%':
-        cout << num1 % num2;
-        break;
-    default:
-        break;
+double calculate(double num1, char operat, double num2) {
+    double result;
+    switch (operat) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                std::cerr << "Error: Division by zero" << std::endl;
+                return 0;
+            }
+            break;
+        default:
+            std::cerr << "Error: Invalid operator" << std::endl;
+            return 0;
     }
+    return result;
+}
+
+int main() {
+    double num1, num2;
+    char operat;
+    std::cin >> num1>> operat>> num2;
+
+    double result = calculate(num1, operat, num2);
+    std::cout << std::fixed << std::setprecision(2) << result << std::endl;
+
     return 0;
 }
